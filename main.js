9f7978d4e8af357ee01f3c34a95e23914ba567d5
Kashyap Panda <5233012+kashyappanda@users.noreply.github.com>
2022-04-21 22:56:25 -0700

    Switched to using the example code from class

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
+var tweets = [];
+
+class Tweet {
+    constructor(text, id, created_at) {
+        this.text = text;
+        this.id = id;
+        this.created_at = created_at;
+    }
+}
+
 window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
-
-    posts = fetchPosts("https://ec2-18-209-247-77.compute-1.amazonaws.com:3000/feed/random?q=weather");
-
-    console.log(posts);
+    setInterval(getTweets, 5000);
 })
 
-async function fetchPosts (url) {
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
+function getTweets() {
+    fetch ('http://ec2-18-209-247-77.compute-1.amazonaws.com:3000/feed/random?q=weather').then(function (response) {
+        return response.json();
+    }).then(function(data) {
+        for (let i = 0; Object.keys(data.statuses).length; i++) {
+            if (data.statuses[i] != null && data.statuses[i].text != null && data.statuses[i].id != null && data.statuses[i].created_at) {
+                let tweet = new Tweet(data.statuses[i].text, data.statuses[i].id, data.statuses[i].created_at);
+                tweets.push(tweet);
+
+                if (tweets.length == 10) {
+                    break;
+                }
+            }
+
+            console.log(tweets);
+            //filtering stuff here
+        }
+
+        console.log(tweets);
+    }).catch(function(err) {
+        console.warn("There was an error fetching the tweets.", err);
+    });
 }
