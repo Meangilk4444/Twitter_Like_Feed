8bb5093d89bb48d0e516b79987d18ad641785c98
Kashyap Panda <5233012+kashyappanda@users.noreply.github.com>
2022-04-23 21:40:50 -0700

    Finished search bar and pause button

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-var tweets = [];
+var unfilteredTweets = [];
+var filteredTweets = [];
 let tweetIDList = [];
+
+let paused = false;
+let searching = false;
+let searchQuery = "";
+
+var tweetList;
 
 class Tweet {
     constructor(text, id, created_at) {
@@ -11,43 +18,84 @@ class Tweet {
 
 window.addEventListener('load', (event) => {
     console.log("Loaded");
+    document.getElementById("searchBar").addEventListener("input", handleSearch);
+    document.getElementById("pause").addEventListener("change", onChecked);
     getTweets();
     setInterval(getTweets, 5000);
 })
 
-function getTweets() {
-    fetch ('http://ec2-18-209-247-77.compute-1.amazonaws.com:3000/feed/random?q=weather').then(function (response) {
-        return response.json();
-    }).then(function(data) {
-        for (let i = 0; Object.keys(data.statuses).length; i++) {
-            if (data.statuses[i] != null && data.statuses[i].text != null && data.statuses[i].id != null && data.statuses[i].created_at) {
-                let tweet = new Tweet(data.statuses[i].text, data.statuses[i].id, data.statuses[i].created_at);
+function onChecked(checkbox) {
+    if (checkbox.target.checked) {
+        paused = true;
+    }
+    else {
+        paused = false;
+    }
+}
 
-                if (!tweetIDList.includes(tweet.id)) {
-                    tweets.push(tweet);
-                    tweetIDList.push(tweet.id);
+const handleSearch = event => {
+    searchQuery = event.target.value.trim().toLowerCase();
+
+    filteredTweets.length = 0;
+    initializeHTML(true);
+
+    if (searchQuery.length == 0) {
+        searching = false;
+        updateFeed(unfilteredTweets);
+    }
+    else {
+        searching = true;
+
+        unfilteredTweets.forEach(element => {
+            if (element.text.trim().toLowerCase().includes(searchQuery)) {
+                filteredTweets.push(element);
+            }
+        });
+
+        updateFeed(filteredTweets);
+    }
+}
+
+function getTweets() {
+    if (!paused) {
+        fetch ('http://ec2-18-209-247-77.compute-1.amazonaws.com:3000/feed/random?q=weather').then(function (response) {
+            return response.json();
+        }).then(function(data) {
+            for (let i = 0; Object.keys(data.statuses).length; i++) {
+                if (data.statuses[i] != null && data.statuses[i].text != null && data.statuses[i].id != null && data.statuses[i].created_at) {
+                    let tweet = new Tweet(data.statuses[i].text, data.statuses[i].id, data.statuses[i].created_at);
+
+                    if (!tweetIDList.includes(tweet.id)) {
+                        unfilteredTweets.push(tweet);
+                        tweetIDList.push(tweet.id);
+                    }
+                }
+
+                if (i == 10) {
+                    break;
                 }
             }
 
-            if (i == 10) {
-                break;
-            }
-        }
+            if (searching) {
+                unfilteredTweets.forEach(element => {
+                    if (element.text.trim().toLowerCase().includes(searchQuery)) {
+                        filteredTweets.push(element);
+                    }
+                });
 
-        updateFeed();
-    }).catch(function(err) {
-        console.warn("There was an error fetching the tweets.", err);
-    });
+                updateFeed(filteredTweets);
+            }
+            else {
+                updateFeed(unfilteredTweets);
+            }
+        }).catch(function(err) {
+            console.warn("There was an error fetching the tweets.", err);
+        });
+    }
 }
 
-function updateFeed() {
-    console.log("constructing feed");
-    var tweetContainer = document.getElementById('tweet-container'); 
-    const tweetList = document.createElement("ul");
-
-    if(tweetContainer != null){
-        tweetContainer.appendChild(tweetList);
-    }
+function updateFeed(tweets) {
+    initializeHTML(false);
 
     //sort by date
     tweets.sort(function compareDates(a,b){
@@ -55,15 +103,6 @@ function updateFeed() {
         if(a.created_at < b.created_at) return -1;
         return 0;
     });
-
-    var j = 0;
-    while( j < tweets.length)
-    {
-        console.log(tweets[j].created_at);
-        j++;
-    }
-    
-
 
     tweets.forEach(tweetObject => {
         // create a container for individual tweet
@@ -83,4 +122,18 @@ function updateFeed() {
         // finally append your tweet into the tweet list
         tweetList.appendChild(tweet);
     });
+}
+
+function initializeHTML(reset) {
+    var tweetContainer = document.getElementById('tweet-container'); 
+    
+    if (reset) {
+        tweetContainer.innerText = '';
+    }
+
+    tweetList = document.createElement("ul");
+
+    if(tweetContainer != null){
+        tweetContainer.appendChild(tweetList);
+    }
 }
